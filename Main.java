ec602316904d134d63f6d493c7261d86c64415b9
Student: Mark Mark <94848722+20079211@users.noreply.github.com>
2022-06-13 13:15:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * This is a class that tests the Deck class.
+ */
+
+// DeckTester.java
+
+public class Main {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+    String[] ranks = {"A", "B", "C"};
+    String[] suits = {"Giraffes", "Lions"};
+    int[] values = {2, 1, 6};
+    /*String[] ranks = {"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"};
+    String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"}; 
+    int[] values = {11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2};*/
+
+    
+    Deck animals = new Deck(ranks, suits, values);
+    
+    System.out.println(animals);
+    System.out.println("\nSize of deck: " + animals.size());
+    
+    System.out.println("\nIs it empty? " + animals.isEmpty());
+    
+    System.out.println("\nCard dealt: " + animals.deal());
+    
+    System.out.println("\nRemaining cards: " + animals.size());
+	}
+}
